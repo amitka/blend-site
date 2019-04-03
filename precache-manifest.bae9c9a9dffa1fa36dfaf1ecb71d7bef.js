11c25243c8e538cbaa637583bf80e20ea08a2f5b
Amitka <amitka111@gmail.com>
2019-04-03 23:42:29 +0300

    Updates

---
+++ precache-manifest.bae9c9a9dffa1fa36dfaf1ecb71d7bef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/amitka/blend-site/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "62f147b7253800833635",
+    "url": "/amitka/blend-site/static/js/runtime~main.53bb9077.js"
+  },
+  {
+    "revision": "9fe426650602bda8444a",
+    "url": "/amitka/blend-site/static/js/main.4ff89d94.chunk.js"
+  },
+  {
+    "revision": "6c79bf4684e99538fd7c",
+    "url": "/amitka/blend-site/static/js/2.8b3d88bd.chunk.js"
+  },
+  {
+    "revision": "9fe426650602bda8444a",
+    "url": "/amitka/blend-site/static/css/main.50a6abdd.chunk.css"
+  },
+  {
+    "revision": "3f0160bc26954de0187f74902c3e5d61",
+    "url": "/amitka/blend-site/index.html"
+  }
+];
